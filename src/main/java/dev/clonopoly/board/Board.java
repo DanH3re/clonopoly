87c0c2a44ca99222ba7875f033d90e5e106da441
Andrei-Chiosa <av.chiosa@gmail.com>
2025-09-28 12:11:12 +0200

    refactor: change board size

--- src/main/java/dev/clonopoly/board/Board.java
+++ src/main/java/dev/clonopoly/board/Board.java
@@ -12,18 +12,13 @@ public class Board {
     private static Board instance = null;
 
     private Board() {
-        this.tiles = new Tile[36];
-        this.size = 36;
-
-        for(int i = 0; i < size; i++) {
-            tiles[i] = new NoOpTile();
-        }
+        this.tiles = new Tile[40];
+        this.size = 40;
 
         tiles[0] = new GoTile();
-        tiles[9] = new JailTile();
-        tiles[18] = new NoOpTile();
-        tiles[27] = new GoToJailTile();
-
+        tiles[10] = new JailTile();
+        tiles[20] = new NoOpTile();
+        tiles[30] = new GoToJailTile();
         //TODO: initialize the tiles with actual game tiles
     }
 
@@ -35,16 +30,18 @@ public class Board {
     }
 
     public Tile tileAt(int row, int col) {
+        int MAX_ROW = size / 4;
+
         int tileNo;
 
-        if (row == 9) {
-            tileNo = 9 - col;
+        if (row == MAX_ROW) {
+            tileNo = MAX_ROW - col;
         } else if (col == 0) {
-            tileNo = 9 + (9 - row);
+            tileNo = MAX_ROW + (MAX_ROW - row);
         } else if (row == 0) {
-            tileNo = 18 + col;
-        } else if (col == 9) {
-            tileNo = 27 + row;
+            tileNo = MAX_ROW*2 + col;
+        } else if (col == MAX_ROW) {
+            tileNo = MAX_ROW*3 + row;
         } else {
             tileNo = -1;
         }
@@ -59,6 +56,10 @@ public class Board {
 
     public void goTo(int pos, Player player) {
         // TODO: moves the player to the specified position
+    }
+
+    public int getSize() {
+        return size;
     }
 
     public void move(int tilesToMove, Player player) {
