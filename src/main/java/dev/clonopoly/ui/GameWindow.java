4154990ba852e019eb5ae5438e766d1bbb1a6986
Dan Gavriluta <dangavriluta3@gmail.com>
2025-09-28 10:11:39 +0200

    feat: use more optimized layout

--- src/main/java/dev/clonopoly/ui/GameWindow.java
+++ src/main/java/dev/clonopoly/ui/GameWindow.java
@@ -1,30 +1,64 @@
 package dev.clonopoly.ui;
+import dev.clonopoly.board.Board;
+
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class GameWindow {
-    JFrame frame = new JFrame();
-    JPanel panelBoard = new JPanel(null);
-    GridLayout gridLayout = new GridLayout(10,10);
+    Board board = Board.getInstance();
+    JFrame windowFrame = new JFrame();
 
-    public GameWindow() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,720);
-        frame.setTitle("Clonopoly");
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    private JFrame initializeFrame() {
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setSize(800,720);
+        windowFrame.setTitle("Clonopoly");
+        windowFrame.setLocationRelativeTo(null);
+        windowFrame.setVisible(true);
+        return windowFrame;
+    }
 
-        panelBoard.setBackground(Color.WHITE);
-        panelBoard.setLayout(gridLayout);
-        //crate 100 more panels to create the board
-        for(int i = 0; i < 100; i++) {
-            JPanel panel = new JPanel();
-            panel.setBackground(Color.WHITE);
-            panel.setBorder(BorderFactory.createLineBorder(Color.black));
-            panelBoard.add(panel);
+    private JPanel initializeBoard() {
+        JPanel boardPanel = new JPanel(null);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                if(row == 0 || row == 9 || col == 0 || col == 9) {
+                    JPanel panel = new JPanel();
+                    panel.setBackground(Color.WHITE);
+                    panel.setBorder(BorderFactory.createLineBorder(Color.black));
+                    gridBagConstraints.gridx = col;
+                    gridBagConstraints.gridy = row;
+                    gridBagConstraints.fill = GridBagConstraints.BOTH;
+                    gridBagConstraints.weightx = 1.0;
+                    gridBagConstraints.weighty = 1.0;
+
+                    JLabel label = new JLabel(board.tileAt(row, col).toString());
+                    panel.add(label);
+                    boardPanel.add(panel, gridBagConstraints);
+                }
+            }
         }
 
-        frame.add(panelBoard);
+        boardPanel.setBackground(Color.WHITE);
+        boardPanel.setLayout(gridBagLayout);
+        boardPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        return boardPanel;
+    }
+
+    public GameWindow() {
+        JFrame windowFrame = initializeFrame();
+        JPanel boardPanel = initializeBoard();
+        windowFrame.add(boardPanel);
+    }
+
+    public void showWIndow() {
+        windowFrame.setVisible(true);
+    }
+
+    public void hideWindow() {
+        windowFrame.setVisible(false);
     }
 }
